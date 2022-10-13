74afbd3a19f52e4d486189294118023393700589
Abdulmenam <93938541+Abdulmenam@users.noreply.github.com>
2022-10-13 10:19:34 +0300

    submit the task

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+    	System.out.println(first>second? first:second);
+    	
     }
 }
